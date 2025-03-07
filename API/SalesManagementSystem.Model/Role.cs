07456408ce9dbf5f637707d1d0ad9fd88df74927
LuisFCosteC <luisfcostec@gmail.com>
2025-03-07 10:43:10 -0500

    We execute the command to obtain the database references in the Model layer.

---
+++ API/SalesManagementSystem.Model/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesManagementSystem.Model;
+
+public partial class Role
+{
+    public int IdRole { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime? DateRegistration { get; set; }
+
+    public virtual ICollection<MenuRole> MenuRoles { get; set; } = new List<MenuRole>();
+
+    public virtual ICollection<Users> Users { get; set; } = new List<Users>();
+}
