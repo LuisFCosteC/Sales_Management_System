a3e5de9cfab04569913a3c954f3ee5fff05b8cc8
LuisFCosteC <luisfcostec@gmail.com>
2025-03-20 10:56:08 -0500

    We create the database dependency, from the API this dependency is called for its execution

---
+++ Api/SalesManagementSystem.IOC/Dependency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesManagementSystem.DAL.DBContext;
+
+namespace SalesManagementSystem.IOC
+{
+    public static class Dependency
+    {
+        public static void InjectDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbSalesContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("stringSQL"));
+            });
+        }
+    }
+}
