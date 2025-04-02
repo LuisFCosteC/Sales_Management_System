32524bff5fe891f25c52d365c172acd14010976c
LuisFCosteC <luisfcostec@gmail.com>
2025-04-02 16:01:29 -0500

    We developed the product module interface.

--- Front-end/src/app/Components/layout/Modals/modal-product/modal-product.component.ts
+++ Front-end/src/app/Components/layout/Modals/modal-product/modal-product.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+// Importaciones necesarias de Angular y otros módulos
+import { Component, OnInit, Inject } from '@angular/core';                  // Importa Component, OnInit y Inject de Angular core
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';        // Importa herramientas para manejar formularios reactivos
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';   // Importa herramientas para manejar diálogos de Angular Material
+
+import { Category } from '../../../../Interfaces/category';
+import { Product } from '../../../../Interfaces/product';
+import { CategoryService } from '../../../../Services/category.service';
+import { ProductService } from '../../../../Services/product.service';
+import { UtilityService } from '../../../../Reusable/utility.service';
 
 @Component({
   selector: 'app-modal-product',
@@ -6,6 +15,90 @@ import { Component } from '@angular/core';
   templateUrl: './modal-product.component.html',
   styleUrl: './modal-product.component.css'
 })
-export class ModalProductComponent {
+export class ModalProductComponent implements OnInit {
+  
+  // Propiedades del componente
+  formsProduct: FormGroup;            
+  titleAction: string = "Agregar";  
+  buttonAction: string = "Save";    
+  listCategory: Category[] = [];    
 
+  constructor(
+    private modalCurent: MatDialogRef<ModalProductComponent>,
+    @Inject(MAT_DIALOG_DATA) public dataProduct: Product,
+    private fb: FormBuilder,
+    private _categoryService: CategoryService,
+    private _productService: ProductService,
+    private _utilityService: UtilityService
+  ) {
+    this.formsProduct = this.fb.group({
+      name: ["", Validators.required],
+      idCategory: ["", Validators.required],
+      stock: ["", Validators.required],
+      price: ["", Validators.required],
+      isActive: ['1', Validators.required],
+    });
+
+    if(this.dataProduct != null) {
+      this.titleAction = "Editar";      // Cambia el título a "Editar"
+      this.buttonAction = "Actualizar"; // Cambia el texto del botón a "Actualizar"
+    }
+
+    this._categoryService.list().subscribe({
+      next: (data) => {
+        if (data.status) this.listCategory = data.value;
+      },
+      error: (e) => {} // Manejo de errores (vacío en este caso)
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.dataProduct != null) {
+      this.formsProduct.patchValue({
+        name: this.dataProduct.name,
+        idCategory: this.dataProduct.idCategory,
+        stock: this.dataProduct.stock,
+        price: this.dataProduct.price,
+        isActive: this.dataProduct.isActive
+      });
+    }
+  }
+
+  SaveEdit_Product() {
+    const _product: Product = {
+      idProduct: this.dataProduct == null ? 0 : this.dataProduct.idProduct,
+      name: this.formsProduct.value.name,
+      idCategory: this.formsProduct.value.idCategory,
+      descriptionCategory: "",
+      price: this.formsProduct.value.price,
+      stock: this.formsProduct.value.price,
+      isActive: parseInt(this.formsProduct.value.isActive)
+    };
+
+    if (this.dataProduct == null) {
+      this._productService.save(_product).subscribe({
+        next: (data) => {
+          if (data.status) {
+            this._utilityService.displayAlert("El producto fue registrado", "Exito");
+            this.modalCurent.close("true");
+          } else {
+            this._utilityService.displayAlert("No se pudo registrar el producto", "Error");
+          }
+        },
+        error: (e) => {}
+      });
+    } else {
+      this._productService.edit(_product).subscribe({
+        next: (data) => {
+          if (data.status) {
+            this._utilityService.displayAlert("El producto fue editado", "Exito");
+            this.modalCurent.close("true");
+          } else {
+            this._utilityService.displayAlert("No se pudo editar el producto", "Error");
+          }
+        },
+        error: (e) => {}
+      });
+    }
+}
 }
--- Front-end/src/app/Components/layout/Pages/product/product.component.ts
+++ Front-end/src/app/Components/layout/Pages/product/product.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalProductComponent } from '../../Modals/modal-product/modal-product.component';
+import { Product } from '../../../../Interfaces/product';
+import { ProductService } from '../../../../Services/product.service';
+import { UtilityService } from '../../../../Reusable/utility.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-product',
@@ -6,6 +14,91 @@ import { Component } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, AfterViewInit{
 
+  tableColumns: string[] = ['name', 'category', 'stock', 'price', 'state', 'actions'];
+  dataStart: Product[] = [];
+  dataListProduct = new MatTableDataSource(this.dataStart);
+  @ViewChild(MatPaginator) tablePagination!: MatPaginator;
+
+  constructor(
+    private dialog: MatDialog,
+    private _productService: ProductService,
+    private _utilityService: UtilityService
+  ) {}
+
+  getProduct() {
+    this._productService.list().subscribe({
+      next: (data) => {
+        if (data.status) {
+          this.dataListProduct.data = data.value;
+        } else {
+          this._utilityService.displayAlert("No se encontraron datos", "Oops!");
+        }
+      },
+      error: (e) => {
+        console.error("Error al obtener los productos:", e);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataListProduct.paginator = this.tablePagination;
+  }
+
+  applyTableFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataListProduct.filter = filterValue.trim().toLocaleLowerCase();
+  }
+
+  
+  newProduct () {
+    this.dialog.open(ModalProductComponent, {
+      disableClose: true
+    }).afterClosed().subscribe(result => {
+      if (result === "true") this.getProduct();
+    });
+  }
+
+  editProduct (product: Product) {
+    this.dialog.open(ModalProductComponent, {
+      disableClose: true,
+      data: product
+    }).afterClosed().subscribe(result => {
+      if (result === "true") this.getProduct();
+    });
+  }
+
+  deleteProduct (product: Product) {
+    Swal.fire({
+      title: '¿Desea eliminar el Producto?',
+      text: product.name,
+      icon: "warning",
+      confirmButtonColor: '#FF8F00',
+      confirmButtonText: "Si, eliminar",
+      showCancelButton: true,
+      cancelButtonColor: '#3085d6',
+      cancelButtonText: 'No, volver'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this._productService.delete(product.idProduct).subscribe({
+          next: (data) => {
+            if (data.status) {
+              this._utilityService.displayAlert("El producto fue eliminado", "Eliminado!");
+              this.getProduct();
+            } else {
+              this._utilityService.displayAlert("No se pudo eliminar el producto", "Error");
+            }
+          },
+          error: (e) => {
+            console.error("Error al eliminar el producto:", e);
+          }
+        });
+      }
+    });
+  }
 }
