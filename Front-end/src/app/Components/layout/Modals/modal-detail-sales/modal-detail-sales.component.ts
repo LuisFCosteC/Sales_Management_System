5c707a7e2890590c447ef4f9fb7ff14ae5c93890
LuisFCosteC <luisfcostec@gmail.com>
2025-04-04 08:52:38 -0500

    We create the logic and the interface in the modal files of the sale detail.

--- Front-end/src/app/Components/layout/Modals/modal-detail-sales/modal-detail-sales.component.ts
+++ Front-end/src/app/Components/layout/Modals/modal-detail-sales/modal-detail-sales.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+// Importaciones necesarias de Angular y otros módulos
+import { Component, OnInit, Inject } from '@angular/core';                  // Importa Component, OnInit y Inject de Angular core
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';   // Importa herramientas para manejar diálogos de Angular Material
+import { Sales } from '../../../../Interfaces/sales';
+import { DetailSales } from '../../../../Interfaces/detail-sales';
 
 @Component({
   selector: 'app-modal-detail-sales',
@@ -6,6 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './modal-detail-sales.component.html',
   styleUrl: './modal-detail-sales.component.css'
 })
-export class ModalDetailSalesComponent {
+export class ModalDetailSalesComponent implements OnInit{
 
+  dateRegistration: string = "";
+  salesNumber: string = "";
+  paymentType: string = "";
+  totalText: string = "";
+  detailSales: DetailSales[]=[];
+  tableColumns: string[] = ['product', 'quantity', 'price', 'total'];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public _sales: Sales) {
+    this.dateRegistration = _sales.dateRegistration!;
+    this.salesNumber = _sales.salesNumber!;
+    this.paymentType = _sales.paymentType;
+    this.totalText = _sales.totalText;
+    this.detailSales = _sales.detailSales;
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
--- Front-end/src/app/Components/layout/Pages/sales-history/sales-history.component.ts
+++ Front-end/src/app/Components/layout/Pages/sales-history/sales-history.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+// Importaciones necesarias para el componente
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';  // Importa Component, OnInit, AfterViewInit y ViewChild de Angular
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';          // Importa herramientas para manejar formularios reactivos
+import { MatTableDataSource } from '@angular/material/table';                 // Importa MatTableDataSource para gestionar datos en tablas
+import { MatPaginator } from '@angular/material/paginator';                   // Importa MatPaginator para paginar los datos en la tabla
+import { MatDialog } from '@angular/material/dialog';                         // Importa MatDialog para abrir diálogos modales
+
+import {  MAT_DATE_FORMATS } from '@angular/material/core';
+import * as moment from 'moment';
+
+import { ModalDetailSalesComponent } from '../../Modals/modal-detail-sales/modal-detail-sales.component';
+import { Sales } from '../../../../Interfaces/sales';
+import { SaleService } from '../../../../Services/sale.service';
+import { UtilityService } from '../../../../Reusable/utility.service';
 
 @Component({
   selector: 'app-sales-history',
@@ -6,6 +19,11 @@ import { Component } from '@angular/core';
   templateUrl: './sales-history.component.html',
   styleUrl: './sales-history.component.css'
 })
-export class SalesHistoryComponent {
+export class SalesHistoryComponent implements OnInit{
 
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 }
